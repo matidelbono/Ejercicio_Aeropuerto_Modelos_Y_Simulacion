9225649ddc3e5a8936f2e619f20e1fb61cc7e183
NachoZ2001 <102260608+NachoZ2001@users.noreply.github.com>
2023-08-21 18:53:51 -0300

    Terminado

--- Aeropuerto/Sistema_Aeropuerto.cc
+++ Aeropuerto/Sistema_Aeropuerto.cc
@@ -1,47 +1,51 @@
-/* Modelo MM1 - Un Servidor, Una Cola  */
-
-/* Modelo MM1 - Un Servidor, Una Cola  */
 
 #include <stdio.h>
 #include <stdlib.h> 
 #include "simlib.c"
 // Definición de eventos
-#define Arribo_Migracion           1  /* Tipo de Evento 1: Arribo Migracion           */
-#define Partida_Aduana			   2  /* Tipo de Evento 2: Partida Aduana*/
-#define Partida_Migracion		   3  /*Tipo de evento  3: Partida Migracion*/
+#define Arribo_Avion				1
+#define Arribo_Aduana				2  /* Tipo de Evento 1: Arribo Aduana */
+#define Partida_Aduana				3  /* Tipo de Evento 2: Partida Aduana */
+#define Partida_Control_Equipaje	4  /*Tipo de evento  3: Partida Migracion */
 
 // Definición de Colas y listas
-#define Agente_1         1  
-#define Agente_2         2 
-#define Agente_Control   3
-#define Cola_Aduana      4
+#define Agente_Aduana_1         1  
+#define Agente_Aduana_2         2 
+#define Agente_Control_3		3
+#define Cola_Aduana				4
 #define Cola_Control_equipaje   5
 
 // Definición Sampst
-#define Media_Demora_colas 
+#define Media_Demora_Cola_Aduana
+#define Media_Demora_Cola_Control_Equipaje
+
 
 /*Definición Posiciones Transfer*/
-#define Tipo_Evento       1
-#define Tiempo_Evento	  2
-#define Agente			  3
-#define Cola              4
+#define Tiempo_Evento			1
+#define Tipo_Evento				2
+#define Cantidad_Pasajeros		3 //Si es necesario
+#define Agente					4 //Si es necesario
+
 
 
 /* Declaraci¢n de variables  */
-bool Control
-int media_interarribos_aduana, media_servicio_no_revision, min_servicio_revision, max_servicio_revision, numero_min_pasajeros, numero_max_pasajeros, media_aviones_por_dia, cantidad_dias; min_numero_aleatorio, max_numero_aleatorio;
-float porcentaje_pasajeros_revisados
+int cod_control, cod_aleatorio, media_interarribos_aduana, media_servicio_no_revision, min_servicio_revision, max_servicio_revision, numero_min_pasajeros, numero_max_pasajeros, media_aviones_por_dia, cantidad_dias; min_numero_aleatorio, max_numero_aleatorio;
+float porcentaje_pasajeros_revisados;
+int contador_revison, contador_pasajeros;
 
 /* Definición Rutinas */
 int main(); /* Main function. */
-void inicializar(void)
-void Rutina_Arribo_Migracion(void)
-void Generar_Arribo_Migracion(void)
-void Rutina_Partida_Aduana(void)
-void Generar_Partida_Aduana(int Agente, bool Control)
-void Rutina_Partida_Control_equipaje(void)
-void Generar_Partida_Control_equipaje(int Agente, bool Control)
-void Reporte();
+void inicializar(void);
+void Rutina_Arribo_Avion(void);
+void Generar_Arribo_Avion(void);
+void Rutina_Arribo_Aduana(void);
+void Generar_Arribo_Aduana(void);
+void Rutina_Partida_Aduana(void);
+void Generar_Partida_Aduana(int Agente, bool Control);
+void Rutina_Partida_Control_equipaje(void);
+void Generar_Partida_Control_equipaje(void);
+void Estadisticas();
+
 // Principal
 int main()
 {
@@ -72,319 +76,235 @@ int main()
 
 		switch (next_event_type)
 		{
-		case Arribo_Migracion:
-			Rutina_Arribo_Migracion();
+		case Arribo_Avion:
+			Rutina_Arribo_Avion();
+			break;
+		case Arribo_Aduana:
+			Rutina_Arribo_Aduana();
 			break;
 		case Partida_Aduana:
 			Rutina_Partida_Aduana();
 			break;
 		case Partida_Control_equipaje:
-			Rutina_Partida_Control_equipaje
-				default
-				break
+			Rutina_Partida_Control_equipaje();
+			break;
 		}
 	}
 
 	/* Llamada al Reporte para mostrar los resultados */
-	reporte();
+	Estadisticas();
 	//fin;
 	system("pause");
 }
 
 /* Inicializar el Sistema */
-void inicializa(void)
+void inicializar(void)
 {
 	// asignar valores  a las variables
-	media_interarribos_aduana = 0, 2;
-	media_servicio_no_revision = 0.55
-		min_servicio_revision = 3
-		max_servicio_revision = 6
-		numero_min_pasajeros = 240
-		numero_max_pasajeros = 350
-		media_aviones_por_dia = 8
-		cantidad_dias = 7
-		min_numero_aleatorio = 1
-		max_numero_aleatorio = 10
-		control = true
+	media_interarribos_aduana = 0.2;
+	media_servicio_no_revision = 0.55;
+	min_servicio_revision = 3;
+	max_servicio_revision = 6;
+	numero_min_pasajeros = 240;
+	numero_max_pasajeros = 350;
+	media_aviones_por_dia = 8;
+	cantidad_dias = 7;
+	min_numero_aleatorio = 1;
+	max_numero_aleatorio = 10;
+	cod_control = 0;
+	cod_aleatorio = ;
 
-		/* Se carga el primer Arribo en la Lista de Eventos */
+	/* Se carga el primer Arribo del primer avion en la Lista de Eventos */
+	Generar_Arribo_Avion();
 
-		transfer[1] = sim_time + expon(media_interarribos, Arribo);
-	transfer[2] = Arribo;
+}
+
+void Rutina_Arribo_Avion(void) {
+	int cantidadPasajeros = transfer[3];
+
+	//Generamos el arribo a aduana
+	Generar_Arribo_Aduana(cantidadPasajeros);
+
+	//Genero proximo arribo avion
+	Generar_Arribo_Avion();
+}
+void Generar_Arribo_Avion(void) {
+	transfer[1] = sim_time + expon(media_interarribos, Arribo_Avion);
+	transfer[2] = Arribo_Avion;
+	transfer[3] = uniform(numero_min_pasajeros, numero_max_pasajeros, Arribo_Avion);
+
 	list_file(INCREASING, LIST_EVENT);
-
 }
-
-
-void Rutina_arribo_Migracion(void)  /* Evento Arribo */
+void Rutina_Arribo_Aduana(void)  /* Evento Arribo */
 {
-	/* Determinar pr¢ximo arribo y cargar en Lista de Eventos */
-	if (Control == false)
+	//Descontar un pasajero
+	int cantidadPasajeros = transfer[3];
+	cantidadPasajeros--;
+
+	//Sumo un pasajero al contador para las estadisticas
+	contador_pasajeros++;
+
+	//Genero el proximo arribo a Aduana
+	if (cantidadPasajeros > 1)
 	{
-		if (list_size[Agente_1] == 0) // El pasajero puede ser atendido por el agente 1
+		Generar_Arribo_Aduana(cantidadPasajeros);
+	}
+
+	//Ver si debe ser atendido rapidamente o ir al control de equipaje
+	cod_aleatorio++; 
+	if (cod_aleatorio == cod_control)
+	{
+		//Debe ir al control de equipaje 
+		cod_control = 0;
+		cod_aleatorio = ;
+
+		//Ver si puede ser atendido
+		if (list_size[Agente_Control_3] == 0)
 		{
-			list_file(FIRST, Agente_1)
-				Generar_Partida_Aduana(Agente_1, false)
-		}
-		else if (list_size[Agente_2] == 0) // El pasajero puede ser atendido por el agente 2
-		{
-			list_file(FIRST, Agente_2)
-				Generar_Partida_Aduana(Agente_2, false)
+			list_file(FIRST, Cola_Control_equipaje);
+
+			//Genero la partida de control equipaje y actualizo demora
+			sampst(0.0, Media_Demora_Cola_Control_Equipaje);
+			Generar_Partida_Control_equipaje();
 		}
 		else
 		{
-			// va a cola_Aduana
-			transfer[Tiempo_Evento] = sim_time
-				list_file(LAST, Cola_Aduana)
-
+			//Mando a cola 
+			transfer[1] = sim_time;
+			list_file(LAST, Cola_Control_equipaje);
 		}
 	}
 	else
 	{
-		if (list_size[Agente_Control] == 0) // El agente de control puede revisar el equipaje
+		//Debe pasar por la aduana rapida
+
+		if (list_size[Agente_Aduana_1] == 0)
 		{
-			list_file(FIRST, Agente_Control)
-			Generar_Partida_Control_equipaje(Agente_Control, true)
+			list_file(FIRST, Agente_Aduana_1);
+
+			//Genero la partida de aduana rapida y actualizo demora
+			sampst(0.0, Media_Demora_Cola_Aduana);
+			Generar_Partida_Aduana(1, cantidadPasajeros);
+		}
+		else if (list_size[Agente_Aduana_2] == 0)
+		{
+
+			list_file(FIRST, Agente_Aduana_2);
+
+			//Genero la partida de aduana rapida
+			sampst(0.0, Media_Demora_Cola_Aduana);
+			Generar_Partida_Aduana(2, cantidadPasajeros);
 		}
 		else
 		{
-			transfer[Tiempo_Evento] = sim_time
-				list_file(LAST, Cola_Control_equipaje)
+			//Va a cola
+			transfer[1] = sim_time;
+			list_file(LAST, Cola_Aduana);
 		}
 	}
+
 }
-void Generar_Arribo_Migracion()
+void Generar_Arribo_Aduana(int cantidadPasajeros)
 {
-	if (Control == false)
-	{
-		transfer[Tiempo_Evento] = sim_time + expon(media_interarribos_aduana, Arribo_Migracion);
-		transfer[Agente] = nro_Agente
-			transfer[Cola] = Cola_Aduana
-	}
-	else
-	{
-		transfer[Tiempo_Evento] = sim_time + expon(media_interarribos_aduana, Arribo_Migracion);
-		transfer[Agente] = Agente_Control
-			transfer[Cola] = Cola_Control_equipaje
-	}
-	transfer[Tipo_Evento] = Arribo_Migracion;
-	list_file(INCREASING, LIST_EVENT)
+	transfer[1] = sim_time + expon(media_interarribos_aduana, Arribo_Aduana);
+	transfer[2] = Arribo_Aduana;
+	transfer[3] = cantidadPasajeros;
+
+	list_file(INCREASING, LIST_EVENT);
 }
 void Rutina_Partida_Aduana()
 {
-	int AgenteAOcupar = 0
-		// Desocupar el agente que atendió al pasajero
-		int NroAgente = transfer[Agente];
-	list_remove(FIRST, NroAgente)
-		int AgenteQueAtendio = 0;
+	// Desocupar el agente que atendió al pasajero
+	int NroAgente = transfer[3];
+	list_remove(FIRST, NroAgente);
 
 	// ver si hay trabajo en cola
 	if (list_size[Cola_Aduana] > 0)
 	{
+		//Saco de la cola
 		list_remove(FIRST, Cola_Aduana);
-		AgenteQueAtendio = transfer[Agente]
-			// Ocupar el agente
-			if (list_size[Agente_1] == 0)
-			{
-				list_file(FIRST, Agente_1);
-				Generar_Partida_Aduana(Agente_1, false);
-			}
-			else
-			{
-				if (list_size[Agente_2]==0)
-				{
-					list_file(FIRST, Agente_2);
-					Generar_Partida_Aduana(Agente_2, false)
-				}
-			}
+
+		//Actualizo demora
+		sampst(sim_time - transfer[1], Media_Demora_Cola_Aduana);
+		
+		//Ocupar agente
+		list_file(FIRST, NroAgente);
+
+
+		//Generar proxima partida aduana
+		Generar_Partida_Aduana(NroAgente);
 	}
 }
 void Generar_Partida_Aduana(int Agente)
 {
-	transfer[Tiempo_Evento] = sim_time + expon(media_servicio_no_revision, Partida_Aduana)
-	transfer[Tipo_Evento] = Partida_Aduana
-	transfer[Agente]=Agente
-	transfer[Cola]=Cola_Aduana
-	list_file(INCREASING, LIST_EVENT)
-}
+	transfer[1] = sim_time + expon(media_servicio_no_revision, Partida_Aduana);
+	transfer[2] = Partida_Aduana;
+	transfer[3] = Agente;
 
-///////////////////////
-/* Chequear si el Servidor est  desocupado */
-
-
-{
-
-	++clientes_act;
-
-	/* Si está desocupado ocuparlo y generar la partida */
-
-	list_file(FIRST, Servidor);
-	sampst(0.0, Demora_cola);
-
-
-	transfer[1] = sim_time + expon(media_servicio, Partida);
-	transfer[2] = Partida;
 	list_file(INCREASING, LIST_EVENT);
-
 }
+void Rutina_Partida_Control_Equipaje(void) {
 
-else
-{
+	//Desocupar al agente
+	list_remove(FIRST, Agente_Control_3);
 
-	/* Si el Servidor est  ocupado poner el Trabajo en Cola */
-
-	transfer[1] = sim_time;
-	list_file(LAST, Cola_Aduana);
-}
-
-}
-
-
-void Rutina_partidas(void)  /* Evento Partida */
-{
-
-	/* Desocupar el Servidor */
-
-	list_remove(FIRST, Servidor);
-
-	/* Ver si hay trabajos en cola */
-
-	if (list_size[Cola] > 0)
+	//ver si hay trabajo en cola
+	if (list_size[Cola_Control_equipaje] > 0)
 	{
+		//Saco el primero de la cola
+		list_remove(FIRST, Cola_Control_equipaje);
 
-		/* Sacar el primero de la cola y actualizar Demoras */
+		//Ocupo el agente
+		list_file(FIRST, Agente_Control_3);
 
-		++clientes_act;
-		list_remove(FIRST, Cola);
-		sampst(sim_time - transfer[1], Demora_cola);
+		//Actualizo demora
+		sampst(sim_time - transfer[1], Media_Demora_Cola_Control_Equipaje);
 
-		/* Cargar en el Servidor y generar la partida */
-		list_file(FIRST, Servidor);
-
-		transfer[1] = sim_time + expon(media_servicio, Partida);
-		transfer[2] = Partida;
-		list_file(INCREASING, LIST_EVENT);
+		//Genero la proxima partida de control equipaje
+		Generar_Partida_Control_Equipaje();
 	}
 }
+void Generar_Partida_Control_Equipaje(void) {
+	
+	//Sumo 1 al contador de pasajeros que fueron al control de equipaje para las estadisticas
+	contador_revison++;
 
+	transfer[1] = sim_time + uniform(min_servicio_revision, max_servicio_revision, Partida_Control_Equipaje);
+	transfer[2] = Partida_Control_Equipaje;
 
-void reporte(void)  /* Generar Reporte de la Simulaci¢n */
-{
-
-	/* Mostrar Par metros de Entrada */
-
-	/* -------- Por pantalla -------- */
-
-	printf("Sistema M/M/1 - Simulaci¢n con Simlib \n\n");
-	printf("Media de Interarribos          : %8.3f minutos\n", media_interarribos);
-	printf("Media de Servicios             : %8.3f minutos\n", media_servicio);
-	printf("Cantidad de Clientes Demorados : %i \n\n", num_clientes);
-
-	/* Calcular los Estad¡sticos */
-
-	/* Estad¡sticos Escalaras - Sampst */
-	sampst(0.0, -Demora_cola);
-	printf("\nDemora en Cola                 : %f \n ", transfer[1]);
-
-
-	/* Estad¡sticos Temporales - Timest y Filest */
-
-	filest(Cola);
-	printf("\nN£mero Promedio en Cola        : %f \n ", transfer[1]);
-	filest(Servidor);
-	printf("\nUtilizaci¢n Servidor           : %f \n ", transfer[1]);
-	printf("\nTiempo Final de Simulation     : %10.3f minutes\n", sim_time);
-
-
-	/* Estad¡sticos Temporales - Timest y Filest */
-
-	filest(Cola);
-	printf("\nN£mero Promedio en Cola        : %f \n ", transfer[1]);
-	filest(Servidor);
-	printf("\nUtilizaci¢n Servidor           : %f \n ", transfer[1]);
-	printf("\nSimulation end time            : %10.3f minutes\n", sim_time);
-
-	   list_file(INCREASING,LIST_EVENT);
-
-	}
-
-	else
-	{
-
-	   /* Si el Servidor est  ocupado poner el Trabajo en Cola */
-
-	   transfer[1] = sim_time;
-	   list_file(LAST, Cola);
-	}
-
+	list_file(INCREASING, LIST_EVENT);
 }
 
+void Estadisticas() {
+	printf("Comienza reporte... \n\n");
+	
+	//Utilizacion de cada agente
+	filest(Agente_Aduana_1);
+	printf("\n Utilizacion Agente de la aduana 1: %f \n ", transfer[1]);
 
-void Rutina_partidas(void)  /* Evento Partida */
-{
+	filest(Agente_Aduana_2);
+	printf("\n Utilizacion Agente de la aduana 2: %f \n ", transfer[1]);
 
-	/* Desocupar el Servidor */
+	filest(Agente_Control_3);
+	printf("\n Utilizacion Agente de control de equipaje: %f \n ", transfer[1]);
 
-	list_remove(FIRST, Servidor);
+	//Demora media de los pasajeros en cola de aduana y cola de control equipaje
+	sampst(0.0, -Media_Demora_Cola_Aduana);
+	printf("\nDemora en cola de aduana: %f \n ", transfer[1]);
 
-	/* Ver si hay trabajos en cola */
+	sampst(0.0, -Media_Demora_Cola_Control_Equipaje);
+	printf("\nDemora en cola de control de equipaje: %f \n ", transfer[1]);
 
-	if (list_size[Cola] > 0)
-	{
+	//Número medio en cola de pasajeros en las dos colas
+	filest(Cola_Aduana);
+	printf("\n Numero Promedio en Cola de aduana: %f \n ", transfer[1]);
 
-	   /* Sacar el primero de la cola y actualizar Demoras */
+	filest(Cola_Control_equipaje);
+	printf("\n Numero Promedio en Cola de control de equipaje: %f \n ", transfer[1]);
 
-	   ++clientes_act;
-	   list_remove(FIRST, Cola);
-	   sampst(sim_time - transfer[1], Demora_cola);
-
-	   /* Cargar en el Servidor y generar la partida */
-	   list_file(FIRST, Servidor);
-
-	   transfer[1] = sim_time + expon(media_servicio, Partida);
-	   transfer[2] = Partida;
-	   list_file(INCREASING,LIST_EVENT);
-	}
-}
-
-
-void reporte( void )  /* Generar Reporte de la Simulaci¢n */
-{
-
-	/* Mostrar Par metros de Entrada */
-
-	/* -------- Por pantalla -------- */
-
-	printf( "Sistema M/M/1 - Simulaci¢n con Simlib \n\n" );
-	printf( "Media de Interarribos          : %8.3f minutos\n", media_interarribos);
-	printf( "Media de Servicios             : %8.3f minutos\n", media_servicio);
-	printf( "Cantidad de Clientes Demorados : %i \n\n", num_clientes);
-
-	/* Calcular los Estad¡sticos */
-
-	/* Estad¡sticos Escalaras - Sampst */
-	sampst(0.0,-Demora_cola);
-	printf( "\nDemora en Cola                 : %f \n ",transfer[1]);
-
-
-	/* Estad¡sticos Temporales - Timest y Filest */
-
-	filest(Cola);
-	printf( "\nN£mero Promedio en Cola        : %f \n ",transfer[1]);
-	filest(Servidor);
-	printf( "\nUtilizaci¢n Servidor           : %f \n ",transfer[1]);
-	printf( "\nTiempo Final de Simulation     : %10.3f minutes\n", sim_time );
-
-
-		/* Estad¡sticos Temporales - Timest y Filest */
-
-	filest(Cola);
-	printf( "\nN£mero Promedio en Cola        : %f \n ",transfer[1]);
-	filest(Servidor);
-	printf( "\nUtilizaci¢n Servidor           : %f \n ",transfer[1]);
-	printf( "\nSimulation end time            : %10.3f minutes\n", sim_time );
-
-
+	//Porcentaje de pasajeros a los que se les revisa el equipaje
+	int porcentaje = (contador_revison * 100) / contador_pasajeros;
+	printf("\n Porcentaje de pasajeros a los que se les revisa el equipaje: %f \n ", porcentaje);
 }
 
 
